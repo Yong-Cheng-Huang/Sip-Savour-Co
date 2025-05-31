329962b53557ba8578f8e1033c41c98f69c9053d
Yong-Cheng <frank0929649689@gmail.com>
2025-05-31 21:37:25 +0800

    Update settings for localization: change LANGUAGE_CODE to Chinese (Traditional) and TIME_ZONE to Asia/Taipei; add STATIC_ROOT for static files management.

--- sipsavour/settings.py
+++ sipsavour/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hant"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Taipei"
 
 USE_I18N = True
 
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
